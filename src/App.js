ce84bc7e8b13e2d681ac1d7cdaaf0e0d2b488b95
JohnMohr <74803311+JohnMohr@users.noreply.github.com>
2021-02-22 17:44:20 -0800

    added imports and return content to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import React, { Component } from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="container-fluid">
-      
-    </div>
-  );
+class App extends Component {
+  return () {
+    return (
+      <div className="big-content">
+        <Layout>
+          <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MohrPortfolio</Link>}>
+            <Navigation>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/resume">Resume</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Header>
+          <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MohrPortfolio</Link>}>
+          <Navigation>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/resume">Resume</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main/>
+          </Content>
+        </Layout>
+      </div>
+
+
+    );
+  }
 }
+    
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import 'react-mdl/extra/material.js';
+import 'react-mdl/extra/material.css';
 
 ReactDOM.render(
   <React.StrictMode>
